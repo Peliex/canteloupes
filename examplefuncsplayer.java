db6c3609b90c6d49e05ce7a3dc2942709b7f33b2
ubbiout <ubbiout@gmail.com>
2018-01-09 17:02:58 -0700

    push test
    starting to made some code

--- examplefuncsplayer.java
+++ examplefuncsplayer.java
@@ -1,17 +1,18 @@
 // import the API.
-// See xxx for the javadocs.
+// See https://s3.amazonaws.com/battlecode-2018/api/java/index.html for the javadocs.
+// Mason thinks we'll get SDK soon... like when the client is released
 import bc.*;
 
 public class Player {
     public static void main(String[] args) {
         // MapLocation is a data structure you'll use a lot.
-        MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
-        System.out.println("loc: "+loc+", one step to the Northwest: "+loc.add(Direction.Northwest));
-        System.out.println("loc x: "+loc.getX());
+        //MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
+        //System.out.println("loc: "+loc+", one step to the Northwest: "+loc.add(Direction.Northwest));
+        //System.out.println("loc x: "+loc.getX());
 
         // One slightly weird thing: some methods are currently static methods on a static class called bc.
         // This will eventually be fixed :/
-        System.out.println("Opposite of " + Direction.North + ": " + bc.bcDirectionOpposite(Direction.North));
+        //System.out.println("Opposite of " + Direction.North + ": " + bc.bcDirectionOpposite(Direction.North));
 
         // Connect to the manager, starting the game
         GameController gc = new GameController();
@@ -19,11 +20,18 @@ public class Player {
         // Direction is a normal java enum.
         Direction[] directions = Direction.values();
 
+        //queue some rocket research on turn 1
+        if(gc.round()==1){
+            gc.queueResearch(bc.UnitType.Rocket ) //M. idk if this is the right way to call queueResearch(UnitType branch)
+        }
+
+        System.out.println("Current round: "+gc.round()); //M. It's it nice to have a computer that will talk to you?
+
+        //M. mine some Karbonite... someime initialKarboniteAt(MapLocation location)
         while (true) {
-            System.out.println("Current round: "+gc.round());
             // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
             VecUnit units = gc.myUnits();
-            for (int i = 0; i < units.size(); i++) {
+            for (int i = 0; i < units.size(); i++) { //M. for each unit in our immutable units vector
                 Unit unit = units.get(i);
 
                 // Most methods on gc take unit IDs, instead of the unit objects themselves.
