01971c36be5b81acad85dd1f1889250f4cbc9825
ubbiout <ubbiout@gmail.com>
2018-01-10 18:52:32 -0700

    Maybe it will run now
    we gave up on having a src directory

--- Player.java
+++ Player.java
@@ -1,18 +1,22 @@
-// import the API.
+//M. Hi everybody. Things weren't working right so Mason (Ubbiout) started over from examplefuncplayer - java
+//M. it's ok nothing i wrote even compiled
 //M. See https://s3.amazonaws.com/battlecode-2018/api/java/index.html for the javadocs.
-//M. Mason (Ubbiout) thinks we'll get SDK soon... like when the client is released
+
+//M. i'm just going to try to put "M." at the beginning of all my comments so u know it was me that fucked that line up or whatever....
+
+// import the API.
 import bc.*;
 
 public class Player {
     public static void main(String[] args) {
         // MapLocation is a data structure you'll use a lot.
-        //MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
-        //System.out.println("loc: "+loc+", one step to the Northwest: "+loc.add(Direction.Northwest));
-        //System.out.println("loc x: "+loc.getX());
+        MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
+        System.out.println("loc: "+loc+", one step to the Northwest: "+loc.add(Direction.Northwest));
+        System.out.println("loc x: "+loc.getX());
 
         // One slightly weird thing: some methods are currently static methods on a static class called bc.
         // This will eventually be fixed :/
-        //System.out.println("Opposite of " + Direction.North + ": " + bc.bcDirectionOpposite(Direction.North));
+        System.out.println("Opposite of " + Direction.East + ": " + bc.bcDirectionOpposite(Direction.East));
 
         // Connect to the manager, starting the game
         GameController gc = new GameController();
@@ -20,24 +24,16 @@ public class Player {
         // Direction is a normal java enum.
         Direction[] directions = Direction.values();
 
-        //queue some rocket research on turn 1
-        if(gc.round()==1){
-            gc.queueResearch(bc.UnitType.Rocket); //M. idk if this is the right way to call queueResearch(UnitType branch)
-        }
-
-        System.out.println("Current round: "+gc.round()); //M. It's it nice to have a computer that will talk to you?
-
-        //M. mine some Karbonite... someime initialKarboniteAt(MapLocation location)
         while (true) {
+            System.out.println("Current round: "+gc.round());
             // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
             VecUnit units = gc.myUnits();
-            for (int i = 0; i < units.size(); i++) { //M. for each unit in our immutable units vector
-                Unit unit = units.get(i); //M. we got that unit object right here
+            for (int i = 0; i < units.size(); i++) {
+                Unit unit = units.get(i);
 
                 // Most methods on gc take unit IDs, instead of the unit objects themselves.
-                //M. isMoveReady(int robot_id)  tests if heat is sufficiently low
-                if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.Southeast)) {
-                    gc.moveRobot(unit.id(), Direction.Southeast);
+                if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.West)) {
+                    gc.moveRobot(unit.id(), Direction.West);
                 }
             }
             // Submit the actions we've done, and wait for our next turn.
@@ -46,9 +42,6 @@ public class Player {
     }
 }
 
-//void moveToLocation(int UnitID, MapLocation){ //M. naieve moveToward(UnitId, MapLoc)
-  
-//}
 
 /* looks like the only map right now has no deposit on it LOL
 public location locateInitialKarboniteDeposits(){
